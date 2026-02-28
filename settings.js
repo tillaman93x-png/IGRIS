f61c0dbe9c3e27a932924b51b671cb2399ec38dc
tillaman93x-png <tillaman93x@gmail.com>
2026-02-28 19:27:12 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'IGRIS THE BLOOD BOT',
+  author: '‎Tillamanx93',
+  botName: "SUNG JINWOO",
+  botOwner: 'ARISE起きる', // Your name
+  ownerNumber: '263777391977', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
